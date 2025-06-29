75e2fa99c23c90a2b08f945c32f327dea3437719
gyanranjan1717 <rgyan931@gmail.com>
2025-06-29 18:06:49 +0530

    image change

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware())
 app.get("/",(req,res) => {
-    res.send("server is live ");
+    res.send("server is live && let me clear ");
 });
 
 app.use('/api/inngest',serve({client:inngest,functions }));
